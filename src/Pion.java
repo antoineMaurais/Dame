c0e5669dd181a54e34173b1aeb0a448dcc906f0f
antoineMaurais <antoine.maurais98@gmail.com>
2022-06-01 17:29:42 +0200

    Ajout de la classe Pion

---
+++ src/Pion.java
@@ -0,0 +1,55 @@
+public class Pion {
+    private int couleur;
+    private int colonne;
+    private int ligne;
+    private boolean vivant;
+    private boolean dame;
+
+    public Pion(final int couleur, final int colonne, final int ligne){
+        this.couleur = couleur;
+        this.colonne = colonne;
+        this.ligne = ligne;
+        this.dame = false;
+        this.vivant = true;
+    }
+
+    public int getCouleur(){
+        return this.couleur;
+    }
+
+    public int getColonne(){
+        return this.colonne;
+    }
+
+    public int getLigne(){
+        return this.ligne;
+    }
+
+    public boolean isDame() {
+        return dame;
+    }
+
+    public boolean isVivant() {
+        return vivant;
+    }
+
+    public void setCouleur(int couleur) {
+        this.couleur = couleur;
+    }
+
+    public void setColonne(int colonne) {
+        this.colonne = colonne;
+    }
+
+    public void setLigne(int ligne) {
+        this.ligne = ligne;
+    }
+
+    public void setDame(boolean dame) {
+        this.dame = dame;
+    }
+
+    public void setVivant(boolean vivant) {
+        this.vivant = vivant;
+    }
+}
