c3fb516ad19f3b6bffd874c011ae1b52eb1e0acb
antoineMaurais <antoine.maurais98@gmail.com>
2022-06-01 18:05:53 +0200

    Ajout de la classe model

---
+++ src/Model.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    private List<Pion> liste;
+
+    public Model(){
+        this.liste = new ArrayList<>();
+        //Pion p = new Pion(1, )
+    }
+}
