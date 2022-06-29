4f2de3a7777491396b9be5a6c4976979c6a15509
Florian Sautejeau <florian.sautejeau@gmail.com>
2022-06-29 12:46:21 +0200

    regarde si il y a qq1 sur les cases en diagonale ou non

--- src/Controller.java
+++ src/Controller.java
@@ -14,55 +14,45 @@ public class Controller {
 
     }
 
-    // A modifier (ne fonctionne pas)
     public void afficherPossibilites(Pion pion) {
+
+        // TODO : Verifier si les cases en diagonale sont vides ou non
+        // Parcourir tous les pions et si il n'y en a pas aux coordonnées des diagonales alors on peut proposer au pion d'avancer sur les deux diagonales
 
         // pions bleu
         if(pion.getCouleur() == 1) {
+            System.out.println("Coordonnées : \n");
+            System.out.println("colonne : " + pion.getColonne());
+            System.out.println("ligne : " + pion.getLigne());
+
 
             List<Pion> pionsBleu = new ArrayList<Pion>();
 
             pionsBleu = model.getPionsCouleur(1);
             for(Pion p: pionsBleu) {
-                if(pion.getColonne()-1 == p.getColonne() && pion.getLigne()-1 == p.getLigne()) {
+                // SI LE TABLEAU possibiliteDeplacement est VIDE, cela veut dire qu'il n'y pas de pion bleu en diagonale
+                if(pion.getColonne()-1 == p.getColonne() && pion.getLigne()+1 == p.getLigne()) {
                     possibiliteDeplacement.add(new Case(1, p.getLigne(), p.getColonne()));
+                    System.out.println(" oui");
                 } else if (pion.getColonne()+1 == p.getColonne() && pion.getLigne()+1 == p.getLigne()) {
-                    possibiliteDeplacement.add(new Case(1, p.getLigne(), p.getColonne()));
-                } else if (pion.getColonne()-1 == p.getColonne() && pion.getLigne()+1 == p.getLigne()) {
-                    possibiliteDeplacement.add(new Case(1, p.getLigne(), p.getColonne()));
-                } else if (pion.getColonne()+1 == p.getColonne() && pion.getLigne()-1 == p.getLigne()) {
                     possibiliteDeplacement.add(new Case(1, p.getLigne(), p.getColonne()));
                 }
             }
             pion.getColonne();
             pion.getLigne();
         } else {
-
+            // Faire pareil pour les pions rouge
         }
 
-        // Ne fonctionne pas
-        System.out.print("deplacement possible :");
+        // Si il n'y a rien dans le tableau c'est que le pion peut avancer normalement
+        System.out.print("deplacement pas possible :");
         for(Case c: possibiliteDeplacement) {
             System.out.println("ligne : " + c.getLigne());
             System.out.println("colonne : " + c.getColonne());
         }
 
-
-       /* if((pion.getCouleur().equals(Couleur.NOIR) && tourNoir) || (p.getCouleur().equals(Couleur.BLANC) && !tourNoir)){
-            int i=0;
-            int j=0;
-            for(int k=0; k<TAILLE*TAILLE; k++){
-                getCase(k).setSelectionnee(false);
-                if(getCase(k).getComponentCount()!=0 && getCase(k).getComponent(0).equals(p)){
-                    caseActive=getCase(k);
-                    i=k/TAILLE;
-                    j=k%TAILLE;
-
-                }
-            }
-            selectionnerCases(i, j, pion.getCouleur());
-        }*/
     }
+
 
     public void deplacerCase(Case caseJeu) {
           /*  caseJeu.add(caseActive.getComponent(0));
